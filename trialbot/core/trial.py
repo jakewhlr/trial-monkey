a52f5da216514afda5f6c7d20532e73845c344ab
Jake Wheeler <jakewhlr@gmail.com>
2019-07-28 19:02:24 -0700

    added toJSON overload to Trial class

--- trialbot/core/trial.py
+++ trialbot/core/trial.py
@@ -1,20 +1,17 @@
 import logging
 import tabulate
 from pyfiglet import figlet_format
-import uuid
 import discord
+import json
 
 class Trial:
 	left_name = ''
 	right_name = ''
 	name = ''
 	standings = {}
-	left = []
-	right = []
-	fence = []
 	status_message = None
 	def __init__(self, left, right):
-		self.id = str(uuid.uuid4())
+		self.id = 0
 		self.left_name = left.capitalize()
 		self.right_name = right.capitalize()
 		self.name = '%s v. %s' % (left.title(), right.title())
@@ -27,11 +24,18 @@ class Trial:
 		return self.name
 
 	def status(self):
-		logging.info(self.left)
-		logging.info(self.fence)
-		logging.info(self.right)
 		logging.info(self.standings)
 		embed = {}
 		embed['title'] = '%s v. %s' % (self.left_name, self.right_name)
 		embed['description'] = '```%s```' % tabulate.tabulate(self.standings, headers = [self.left_name, 'The Fence', self.right_name], tablefmt='grid')
 		return discord.Embed.from_dict(embed)
+
+	def toJSON(self):
+		output = {
+			"name": self.name,
+			"id": self.id,
+			"left_name": self.left_name,
+			"right_name": self.right_name,
+			"standings": self.standings
+		}
+		return json.dumps(output)
