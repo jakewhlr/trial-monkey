094bce30e4fc89468ce26ff79566d6ca09b75770
Jake Wheeler <jakewhlr@gmail.com>
2019-08-02 20:11:59 -0700

    added trial.status()

--- trialbot/core/trial.py
+++ trialbot/core/trial.py
@@ -9,12 +9,17 @@ class Trial:
 	right_name = ''
 	name = ''
 	standings = {}
+	votes = {}
 	status_message = None
 	def __init__(self, left, right):
 		self.id = 0
 		self.left_name = left.capitalize()
 		self.right_name = right.capitalize()
 		self.name = '%s v. %s' % (left.title(), right.title())
+		self.description = ""
+		self.votes[self.left_name.lower()] = []
+		self.votes['fence'] = []
+		self.votes[self.right_name.lower()] = []
 		self.standings['left'] = []
 		self.standings['fence'] = []
 		self.standings['right'] = []
@@ -24,42 +29,51 @@ class Trial:
 		return self.name
 
 	def vote(self, team, username):
-		if team not in self.standings:
+		team = team.lower()
+		if team not in self.votes:
 			return 1
-		elif username in self.standings[team]:
+		elif username in self.votes[team]:
 			return 0
 		else:
-			if username in self.standings['left']:
-				self.standings['left'].remove(username)
-			if username in self.standings['fence']:
-				self.standings['fence'].remove(username)
-			if username in self.standings['right']:
-				self.standings['right'].remove(username)
-			self.standings[team].append(username)
+			for current_team in self.votes.keys():
+				if username in self.votes[current_team]:
+					self.votes[current_team].remove(username)
+			self.votes[team].append(username)
 			return 0
 
 	def status(self):
-		logging.info(self.standings)
-		embed = discord.Embed()
-		embed.title = '%s v. %s' % (self.left_name, self.right_name)
-		# embed.description = '```%s```' % tabulate.tabulate(self.standings, headers = [self.left_name, 'The Fence', self.right_name], tablefmt='grid')
-		print('\n'.join(self.standings['left']))
-		if self.standings['left']:
-			value = '\n'.join(self.standings['left'])
-		else:
-			value = '‌‌ '
-		embed.add_field(name="__" + self.left_name + "__", value=value)
-		if self.standings['fence']:
-			value = '\n'.join(self.standings['fence'])
-		else:
-			value = '‌‌ '
-		embed.add_field(name="__The Fence__", value=value)
-		if self.standings['right']:
-			value = '\n'.join(self.standings['right'])
-		else:
-			value = '‌‌ '
-		embed.add_field(name="__" + self.right_name + "__", value=value)
-		return embed
+		output = {}
+		title = ""
+		for item in self.votes.keys():
+			if item != "fence":
+				title = title + "%s v. " % item.title()
+		if title.endswith(" v. "):
+			title = title[:-4]
+		output['title'] = title
+		output['description'] = self.description
+		output['votes'] = self.votes
+		return output
+		# logging.info(self.standings)
+		# embed = discord.Embed()
+		# embed.title = '%s v. %s' % (self.left_name, self.right_name)
+		# # embed.description = '```%s```' % tabulate.tabulate(self.standings, headers = [self.left_name, 'The Fence', self.right_name], tablefmt='grid')
+		# print('\n'.join(self.standings['left']))
+		# if self.standings['left']:
+		# 	value = '\n'.join(self.standings['left'])
+		# else:
+		# 	value = '‌‌ '
+		# embed.add_field(name="__" + self.left_name + "__", value=value)
+		# if self.standings['fence']:
+		# 	value = '\n'.join(self.standings['fence'])
+		# else:
+		# 	value = '‌‌ '
+		# embed.add_field(name="__The Fence__", value=value)
+		# if self.standings['right']:
+		# 	value = '\n'.join(self.standings['right'])
+		# else:
+		# 	value = '‌‌ '
+		# embed.add_field(name="__" + self.right_name + "__", value=value)
+		# return embed
 
 	def toJSON(self):
 		output = {
