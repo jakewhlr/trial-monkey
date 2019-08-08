69e7f87bb7beab69e5cd22df1f5e8cca3072e713
Jake Wheeler <jakewhlr@gmail.com>
2019-08-07 22:06:28 -0700

    added check_valid_reaction test

--- tests/core/test_bot.py
+++ tests/core/test_bot.py
@@ -18,14 +18,48 @@ class TestTrialBot(object):
 		assert test_bot.bot.command_prefix == "test_command_prefix"
 		del test_bot
 
-	# def test_bot_reacton_add(self):
-	# 	test_bot = TrialBot("afj34q9opf8ihaf44f8hasef")
-	# 	bot_user_id = 1
-	# 	status_message_id = 16239874
-	# 	reaction_info = {
-	# 		"message_id": 0,
-	# 		"message_author_id": 1234,
-	# 		"emoji": "1⃣"
-	# 	}
-	# 	response_code = test_bot.bot_on_reaction_add(bot_user_id, status_message_id, reaction_info)
-	# 	assert response_code == 0
+	def test_check_valid_reaction(self):
+		test_bot = TrialBot("afj34q9opf8ihaf44f8hasef")
+
+		# TODO - Write bot.new_trial() function + test
+		# Test case 4 depends ^
+
+		# Test case 1: Message reacted != status message
+		response = test_bot.check_valid_reaction(
+			bot_user_id = 1234,
+			status_message_id = 169851,
+			message_id = 19681,
+			user_id = 6514,
+			emoji = '1⃣'
+		)
+		assert response == False
+
+		# Test case 2: Bot user == reacting user
+		response = test_bot.check_valid_reaction(
+			bot_user_id = 1234,
+			status_message_id = 169851,
+			message_id = 169851,
+			user_id = 1234,
+			emoji = '1⃣'
+		)
+		assert response == False
+
+		# Test case 3: Emoji not in assigned_emoji
+		response = test_bot.check_valid_reaction(
+			bot_user_id = 1234,
+			status_message_id = 169851,
+			message_id = 169851,
+			user_id = 12345,
+			emoji = '😎'
+		)
+		assert response == False
+
+		# Test case 4: All parameters pass
+		response = test_bot.check_valid_reaction(
+			bot_user_id = 1234,
+			status_message_id = 19681,
+			message_id = 19681,
+			user_id = 12345,
+			emoji = '1⃣'
+		)
+		assert response == True
